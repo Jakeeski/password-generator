3b315d7b3d0a365a86a22726fce7528397a1bae6
Jakeeski <127697648+Jakeeski@users.noreply.github.com>
2023-04-02 13:21:54 -0500

    good start on project. Added variables and functions.

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -13,5 +13,40 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = ""
+var numeric = ""
+var specialCharacters = ""
+var temporaryPassword= ""
+var password = ""
 
-//testing repo
+
+function generatePassword() {
+  var passwordLength = prompt("How long do you want your password?(Between 8-128 Characters");
+  if(passwordLength < 8){
+    alert("Must be at least 8 characters long")
+    return null;
+  }
+  if(passwordLength > 128) {
+    alert("Must be less than 128 characters")
+    return null;
+  }
+  console.log(passwordLength);
+  var lowercaseCharacters = confirm("Would you like lowercase characters?");
+  console.log(lowercaseCharacters);
+  if(lowercaseCharacters){
+    temporaryPassword+=lowercase;
+  }
+  console.log(temporaryPassword);
+
+  for (var i = 0; i < passwordLength; i++) {
+    password += temporaryPassword.charAt(Math.floor(Math.random() * temporaryPassword.length));
+  }
+
+return password;
+
+
+
+}
+
+
